0a0fc9653c329d7ae232857de87579f39d42927c
LilianaAmangoua <amangoua99@gmail.com>
2024-12-06 18:16:00 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/home/greta/Documents/new-racines-boucles/index.html",
+    "/home/greta/Documents/new-racines-boucles/script.js"],
+  theme: {
+    extend: {
+      colors : {
+        'black' : '#3c3c3c',
+        'white' : '#f8f8f8',
+        'fuschia' : '#FF00A8',
+        'light-pink' : '#FFC1CC',
+      }
+    },
+  },
+  plugins: [],
+}
+
